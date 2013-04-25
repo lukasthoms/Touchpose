dc8ea82a6f0db1f7852c06dc4a3e67c4fc2a5248
Yaron Inger <yaron@lightricks.com>
2013-04-25 11:39:39 +0300

    Compile Touchpose on debug build configuration only.

--- Touchposé Example/Source/QTouchposeApplication.h
+++ Touchposé Example/Source/QTouchposeApplication.h
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#if defined(TOUCHPOSE) && TOUCHPOSE
+#if defined(DEBUG)
 
 #import <UIKit/UIKit.h>
 
--- Touchposé Example/Source/QTouchposeApplication.m
+++ Touchposé Example/Source/QTouchposeApplication.m
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#if defined(TOUCHPOSE) && TOUCHPOSE
+#if defined(DEBUG)
 
 #import "QTouchposeApplication.h"
 #import <QuartzCore/QuartzCore.h>
